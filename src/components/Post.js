e3f7a4d8e52b40dc1aae6a1d1e2c0fdfe6aa6633
sakshi mishra <78993444+sakshi8299@users.noreply.github.com>
2022-06-25 10:08:31 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,26 +10,26 @@ function Post() {
     <div className="post">
       
         <div className="post__header">
-        <ListItem>
+               <ListItem>
                     <ListItemAvatar>
-                        <Avatar className="post__avatar" src="https://source.unsplash.com/random" alt="User" />
+                    <Avatar className="post__avatar" src="https://source.unsplash.com/random" alt="User" />
                     </ListItemAvatar>
                     <ListItemText primary="curicity" secondary="Milkcow Canada" />
-                </ListItem>
-                <img
+               </ListItem>
+                   <img
                     className="post__image"
                     src="https://images.unsplash.com/photo-1609220361638-14ceb45e5e1e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
                     alt="Post pic"
-                />
-                <h4 className="post__text"><strong>curicity: </strong>happy to see you there😍😘🤩</h4> 
+                    />
+                   <h4 className="post__text"><strong>curicity: </strong>happy to see you there😍😘🤩</h4> 
              
-                <div className="post__comments">
+                  <div className="post__comments">
                     <Comments/>
-                </div>
+                  </div>
             
-        </div>
-        </div>
-  )
+            </div>
+     </div>
+        )
 }
 
 export default Post
